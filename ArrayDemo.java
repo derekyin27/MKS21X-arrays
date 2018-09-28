66a500c3bd7e87046fc57fa5b262a5a31d3ae2db
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-09-27 23:50:22 -0400

    Update ArrayDemo.java

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,28 +1,51 @@
 Public class ArrayDemo{
-public static String printersArray(int[]ary){
-  String printers = "[";
+public static void PrintArray(int[]ary){
   for (int i = 0; i < ary.length; i++) {
-    printers = printers + ary[i] + ",";
-  }
-  printers = printers + ary[ary.length-1] + "]";
-  return printers;
+    System.out.print(ary[i] + " ");
+}
 }
   
-  public static void PrintArray(int[]ary){
-    System.out.println(printersArray(ary));
-}
-  
-  public static String printersBIGArray(int[][]ary){
-    String printers = "[";
-    for (int i = 0; i <ary[][].length; i++){
-      printers = printers + printersArray(ary[i]);
+  public static String PrintArray(int[][]ary){
+    int c = 0;
+    for (int i = 0; i < ary.length; i++){
+      for (int p = 0; p < ary[i].length; p++) {
+        System.out.print(ary[i][p] + " ");
     }
-    return printers + "]";
+      System.out.print(" ");
+    }
   }
   
-  public static void PrintBigArray(int[][]ary){
-    System.out.println(printersBIGArray(ary));
+  
+  public static int CountZeros2D(int[][] nums){
+    int c = 0;
+    for (int i = 0; i < nums.length; i++){
+      for (int p = 0; p < nums[i].length; p++) {
+        if (nums[i][p] == 0) {
+          c++;
+        }
+      }
+    }
   }
+  
+  public static void fill2D(int[][] vals){
+    for (int i = 0; i < vals.length; i++){
+      for (int p = 0; p < vals[i].length; p++) {
+        if (i == p) {
+          vals[i][p] = 3;
+        }
+        else {
+          vals[i][p] = 1;
+        }
+      }
+    }
+    printArray(vals);
+  }
+  
+  public static int[][] fill2DCopy(int[][] vals){
+    
+  
+      
+        
   
   
       

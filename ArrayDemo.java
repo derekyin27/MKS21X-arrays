3b40707a4c31aa911f6d806bd8d93fc69e7a5f91
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-09-27 21:54:00 -0400

    Update ArrayDemo.java

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,1 +1,12 @@
+public static void printArray(int[]ary){
+  int[] printer = new int[ary.length];
+  for (int i = 0; i < ary.length; i++) {
+    printer[i] = ary[i];
+  }
+  return printer;
+}
 
+public static void printArray(int[][]ary){
+  int[][] printer1 = new int[ary.length];
+  for (int i = 0; i < ary
+    
